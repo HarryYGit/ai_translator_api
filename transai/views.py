88b06b46bd9f750eacc4ce6518edd67048f5162f
sorryhoney <yharrytsy@gmail.com>
2024-10-09 15:39:40 +1100

    add model for trans data and change to gerericapi

--- transai/views.py
+++ transai/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 
-
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+from rest_framework import serializers
+from rest_framework.generics import GenericAPIView
+
+from .models import TransData
 
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
-
-
 
 # Create your views here.
 
@@ -17,21 +18,83 @@ tokenizer_nllb = AutoTokenizer.from_pretrained('facebook/nllb-200-distilled-600M
 model_nllb = AutoModelForSeq2SeqLM.from_pretrained('facebook/nllb-200-distilled-600M')
 translation_pipeline = pipeline('translation', model=model_nllb, tokenizer=tokenizer_nllb, max_length=200)
 
+def translator(source, source_lang, target_lang):
+    source_text = source
+    source_lang = source_lang
+    target_lang = target_lang
+
+    output = translation_pipeline(source_text, src_lang=source_lang, tgt_lang=target_lang)[0]['translation_text']
+
+    return output
+    
+
+
+
+class TransaiSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = TransData
+        fields = "__all__"
+
+# # defin api
+# class TranslateView(APIView):
+
+#     def post(self, request):
+#         source_text = request.data.get('source_text')
+#         source_lang = request.data.get('source_lang', 'zho_Hans')
+#         target_lang = request.data.get('target_lang', 'eng_Latn')
+        
+
+#         if not source_text:
+#             return Response({'error':'Source text is required'}, status=status.HTTP_400_BAD_REQUEST)
+        
+#         translated =  translator(source_text, source_lang, target_lang)
+   
+#         return Response({
+
+#             'source_text': source_text,
+#             'translated_text': translated,
+            
+#         }, status=status.HTTP_201_CREATED)
+
+
+
 # defin api
-class TranslateView(APIView):
+class TranslateView(GenericAPIView):
+    queryset = TransData.objects.all()
+    serializer_class = TransaiSerializers
+
+    def get(self, request):
+
+        serializer = self.get_serializer(instance=self.get_queryset(), many=True)
+
+        return Response(serializer.data)
 
     def post(self, request):
-        source_text = request.data.get('source_text')
-        source_lang = request.data.get('source_lang', 'zho_Hans')
-        target_lang = request.data.get('target_lang', 'eng_Latn')
 
-        if not source_text:
-            return Response({'error':'Source text is required'}, status=status.HTTP_400_BAD_REQUEST)
+        serializer = self.get_serializer(data = request.data)
+
+        if serializer.is_valid():
+                source_text_input = serializer.validated_data.get('source_text')
+                source_lang_input = request.data.get('source_lang', 'zho_Hans')
+                target_lang_input = request.data.get('target_lang', 'eng_Latn')
+
+                if not source_text_input:
+                    return Response({'error':'Source text is required'}, status=status.HTTP_400_BAD_REQUEST)
         
-        translated = translation_pipeline(source_text, src_lang=source_lang, tgt_lang=target_lang)[0]['translation_text']
+                translated =  translator(source_text_input, source_lang_input, target_lang_input)
 
-        return Response({
+                trans_data = TransData.objects.create(
+                     
+                     source_text = source_text_input,
+                     source_lang = source_lang_input,
+                     target_lang = target_lang_input,
+                     trans_text = translated
+                )
 
-            'source_text': source_text,
-            'translated_text': translated,
-        }, status=status.HTTP_201_CREATED)
+                response_serializer = self.get_serializer(trans_data)
+                return Response(response_serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+                
+
