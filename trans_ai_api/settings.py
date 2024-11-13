dae8c79de9c88fd1aebf0fb4346610f2941f3b3e
HarryYGit <yharrytsy@gmail.com>
2024-11-13 13:54:45 +1100

    modify settings for production

--- trans_ai_api/settings.py
+++ trans_ai_api/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-oc$kfn&p+@uydyf)l!&9c-@jnr*@pn=jvmdtp!2&!4@9kdxlxt"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
